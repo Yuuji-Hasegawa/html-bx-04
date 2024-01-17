8189d3dc60ef8b700b60859791081e2515e97113
Yuuji Hasegawa <hasegawa@kamenwriter.com>
2024-01-17 10:56:39 +0900

    build bx-04

--- src/scripts/main.mjs
+++ src/scripts/main.mjs
@@ -7,9 +7,31 @@ lazyMap();
 import { shareForm } from "./shareForm.js";
 shareForm();
 
-/*
+import Swiper from "swiper";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
 
-import { sidemenu } from "./sidemenu.js";
-sidemenu();
-
-*/
+const swiper = new Swiper(".swiper", {
+  modules: [Navigation, Pagination, Autoplay],
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  speed: 500,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    waitForTransition: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
